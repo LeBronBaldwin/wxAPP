b65995552a65b206961d83c3eca4b24b8493dd9b
unknown <unknown@example.com>
2020-11-30 08:44:28 +0800

    11/30

--- miniprogram/pages/KXYK/KXYK.js
+++ miniprogram/pages/KXYK/KXYK.js
@@ -1,89 +1,106 @@
+// pages/KXYK/KXYK.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+array:[],
+tiaoshu:'10'
   },
-HL:function(){
-  wx.navigateTo({
-    url: '/pages/HL/HL'
-  })
+shuaxin:function(){
+  var that=this
+  wx.request({
+  url: 'http://api.tianapi.com/txapi/joke/index',
+  method:'GET',
+  data:{
+    key:'0baf3813d541a3fdcb85a55baaa05b9f',
+    num:that.data.tiaoshu
+  },
+  success:function(res){
+    console.log(res.data)
+that.setData({
+  array:res.data.newslist
+})
+  }
+})
+
 },
-XZYS:function(){
-  wx.navigateTo({
-    url: '/pages/XZYS/XZYS'
+input(e){
+  console.log(e)
+  var tiaoshu=e.detail.value
+  this.setData({
+    tiaoshu:tiaoshu
   })
-},
-TQ:function(){
-  wx.navigateTo({
-    url: '/pages/TQ/TQ'
-  })
-},
-SS:function(){
-  wx.navigateTo({
-    url: '/pages/SS/SS'
-  })
-},
-KXYK:function(){
-  wx.navigateTo({
-    url: '/pages/KXYK/KXYK'
-  })
-},
+      },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that=this
+    wx.request({
+    url: 'http://api.tianapi.com/txapi/joke/index',
+    method:'GET',
+    data:{
+      key:'0baf3813d541a3fdcb85a55baaa05b9f',
+      num:'10'
+    },
+    success:function(res){
+      console.log(res.data)
+  that.setData({
+    array:res.data.newslist
+  })
+    }
+  })
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
