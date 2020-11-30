1549165977fd6005bdd85f145351911ea9a6daae
unknown <unknown@example.com>
2020-11-30 10:03:14 +0800

    11/30

--- miniprogram/pages/GJXW/GJXW.js
+++ miniprogram/pages/GJXW/GJXW.js
@@ -1,94 +1,66 @@
+// pages/GJXW/GJXW.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
-HL:function(){
-  wx.navigateTo({
-    url: '/pages/HL/HL'
-  })
-},
-XZYS:function(){
-  wx.navigateTo({
-    url: '/pages/XZYS/XZYS'
-  })
-},
-TQ:function(){
-  wx.navigateTo({
-    url: '/pages/TQ/TQ'
-  })
-},
-SS:function(){
-  wx.navigateTo({
-    url: '/pages/SS/SS'
-  })
-},
-KXYK:function(){
-  wx.navigateTo({
-    url: '/pages/KXYK/KXYK'
-  })
-},
-XW:function(){
-  wx.navigateTo({
-    url: '/pages/XW/XW'
-})
-},
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
