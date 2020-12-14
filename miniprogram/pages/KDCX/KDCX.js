519e0481e65eafc8773ccdf3671a0d359a3f16af
unknown <unknown@example.com>
2020-12-14 22:38:27 +0800

    12/14

--- miniprogram/pages/KDCX/KDCX.js
+++ miniprogram/pages/KDCX/KDCX.js
@@ -1,110 +1,89 @@
+// pages/KDCX/KDCX.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+danhao:'',
+newslist:[],
   },
-HL:function(){
-  wx.navigateTo({
-    url: '/pages/HL/HL'
-  })
-},
-XZYS:function(){
-  wx.navigateTo({
-    url: '/pages/XZYS/XZYS'
-  })
-},
-TQ:function(){
-  wx.navigateTo({
-    url: '/pages/TQ/TQ'
-  })
-},
-SS:function(){
-  wx.navigateTo({
-    url: '/pages/SS/SS'
-  })
-},
-KXYK:function(){
-  wx.navigateTo({
-    url: '/pages/KXYK/KXYK'
-  })
-},
-XW:function(){
-  wx.navigateTo({
-    url: '/pages/XW/XW'
-})
-},
-KB:function(){
-  wx.navigateTo({
-    url: '/pages/KB/KB'
-})
-},
-ZGJM:function(){
-  wx.navigateTo({
-    url: '/pages/ZGJM/ZGJM'
-})
-},
-KDCX:function(){
-  wx.navigateTo({
-    url: '/pages/KDCX/KDCX'
-})
-},
+  input(e){
+    console.log(e)
+    var time=e.detail.value
+    this.setData({
+      danhao:time
+    })
+        },
+        chaxun:function(){
+          var that=this
+          wx.request({
+          url: 'https://v1.alapi.cn/api/kd',
+          method:'GET',
+          data:{
+            number:this.data.danhao,
+          },
+          success:function(res){
+            console.log(res)
+            that.setData({
+              newslist:res.data.data.info,
+            })
+          }
+        })
+        },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  },
 
+  }
 })
--- miniprogram/pages/ZGJM/ZGJM.js
+++ miniprogram/pages/ZGJM/ZGJM.js
@@ -1,110 +1,91 @@
+// pages/ZGJM/ZGJM.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+  meng:'',
+  newslist:[],
   },
-HL:function(){
-  wx.navigateTo({
-    url: '/pages/HL/HL'
+  chaxun:function(){
+    var that=this
+    wx.request({
+    url: 'http://v.juhe.cn/dream/query?key=21d2c40ecc468b9912572682fd9ce315&q=%E9%BB%84%E9%87%91',
+    method:'GET',
+    data:{
+      key:'21d2c40ecc468b9912572682fd9ce315',
+      q:this.data.meng,
+      full:'1'
+    },
+    success:function(res){
+      console.log(res.data)
+      that.setData({
+        newslist:res.data.result
+      })
+    }
   })
-},
-XZYS:function(){
-  wx.navigateTo({
-    url: '/pages/XZYS/XZYS'
-  })
-},
-TQ:function(){
-  wx.navigateTo({
-    url: '/pages/TQ/TQ'
-  })
-},
-SS:function(){
-  wx.navigateTo({
-    url: '/pages/SS/SS'
-  })
-},
-KXYK:function(){
-  wx.navigateTo({
-    url: '/pages/KXYK/KXYK'
-  })
-},
-XW:function(){
-  wx.navigateTo({
-    url: '/pages/XW/XW'
-})
-},
-KB:function(){
-  wx.navigateTo({
-    url: '/pages/KB/KB'
-})
-},
-ZGJM:function(){
-  wx.navigateTo({
-    url: '/pages/ZGJM/ZGJM'
-})
-},
-KDCX:function(){
-  wx.navigateTo({
-    url: '/pages/KDCX/KDCX'
-})
-},
+  },
+  input(e){
+    console.log(e)
+    var time=e.detail.value
+    this.setData({
+      meng:time
+    })
+        },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  },
 
+  }
 })
